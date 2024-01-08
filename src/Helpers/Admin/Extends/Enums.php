f4a9ebc09d6b0bf7adef378099e2d228f809343a
liushoukun <liushoukun66@gmail.com>
2024-01-08 23:06:53 +0800

    feat: test

--- src/Helpers/Admin/Extends/Enums.php
+++ src/Helpers/Admin/Extends/Enums.php
@@ -10,31 +10,26 @@ class Enums extends AbstractDisplayer
 {
     public function display($options = [], $default = null) : string
     {
-        $enums = $this->value;
+        $enum = $this->value;
         if (is_null($this->value)) {
             return '';
         }
 
         $value = is_object($this->value) ? $this->value->value : $this->value;
 
-        $colors = Admin::color()->all();
-
         $index = $value;
-
         if (is_string($value)) {
-            $index = sprintf('%u', crc32($value)) % 10;
+            $index = sprintf('%u', crc32($value)) % 5;
         }
-
+        $colors     = Admin::color()->all();
         $style      = array_keys($colors)[(int)$index] ?? 'default';
         $background = Admin::color()->get($style, $style);
-
         try {
-            $name = $enums->name();
+            $name = $enum->label();
         } catch (\Throwable) {
             $name = (string)$value;
 
         }
-
 
         return "<i class='fa fa-circle' style='font-size: 13px;color: {$background}'></i>&nbsp;&nbsp;" . $name;
 
